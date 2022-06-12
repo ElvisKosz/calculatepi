53dc506dbd6a772f28a402e81f4f74fea039d7fd
ElvisKosz <75527875+ElvisKosz@users.noreply.github.com>
2022-06-12 17:53:06 +0200

    Update calculatepi.py

--- calculatepi.py
+++ calculatepi.py
@@ -1,9 +1,8 @@
-x = 1
-pi = 1
-tPi = 0
-for i in range(999999999):
-  pi =tPi + 1/x - 1/(x+2)
-  x += 4
-  tPi = pi
-  print(pi*4)
+pi = 3
+temporary = 2
+
+while True:
+  pi = pi + 4/(temporary*(temporary+1)*(temporary+2)) - 4/((temporary+2)*(temporary+3)*(temporary+4))
+  temporary = temporary + 4
+  print(pi)
   
